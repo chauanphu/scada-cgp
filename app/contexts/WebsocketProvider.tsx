0d678d542b5e2886623531ea57d3f73361538f12
Lê Thành Danh - Nova <ITITWE23014@student.hcmiu.edu.vn>
2024-10-23 02:53:14 +0700

    Danh } Awaiting fix
    What to fix : Unit and UnitStatus is 2 different kind of type, so it create a conflix

--- app/contexts/WebsocketProvider.tsx
+++ app/contexts/WebsocketProvider.tsx
@@ -1,3 +1,4 @@
+// app/contexts/WebsocketProvider.tsx
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { useAPI } from './APIProvider';
 import { NEXT_PUBLIC_WS_URL } from '@/lib/api';
@@ -9,17 +10,21 @@ interface WebSocketProviderProps {
 }
 
 export type UnitStatus = {
+    id: number;
     isOn: boolean;
     isConnected: boolean;
     power: number;
     current: number;
     voltage: number;
+    gps_log: number;
+    gps_lat: number;
 };
 
 export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }) => {
     const { token, isAuthenticated, clusters } = useAPI();
     const [sockets, setSockets] = useState<Map<number, WebSocket>>(new Map());
     const [unitStatus, setUnitStatus] = useState<Record<number, UnitStatus>>({});
+    const [selectedUnit, setSelectedUnit] = useState<UnitStatus | null>(null);
 
     useEffect(() => {
         const connectWebSocket = (unitId: number) => {
@@ -37,11 +42,14 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }
                 setUnitStatus((prevState) => ({
                     ...prevState,
                     [unitId]: { 
+                        id: unitId,
                         isOn, 
                         isConnected,
                         power: data.power,
                         current: data.current,
                         voltage: data.voltage,
+                        gps_log: data.gps_log,
+                        gps_lat: data.gps_lat,
                     },
                 }));
 
@@ -55,6 +63,8 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }
                             power: 0,
                             current: 0,
                             voltage: 0,
+                            gps_log: 0,
+                            gps_lat: 0,
                         },
                     }));
                 }
@@ -102,20 +112,16 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }
 
     const toggleLight = (unitId: number) => {
         const status = unitStatus[unitId];
-        console.log("Unit id: ", unitId)
-        console.log("Unit: ", status)
         if (status) {
-            // sendMessage(unitId, JSON.stringify({ toggle: status.isOn ? 0 : 1 }));
             setUnitStatus((prevState) => ({
                 ...prevState,
                 [unitId]: { ...status, isOn: !status.isOn },
             }));
-            console.log("Unit: ", unitStatus)
         }
     };
 
     return (
-        <WebSocketContext.Provider value={{ unitStatus, toggleLight, sendMessage }}>
+        <WebSocketContext.Provider value={{ unitStatus, selectedUnit, setSelectedUnit, toggleLight, sendMessage }}>
             {children}
         </WebSocketContext.Provider>
     );
