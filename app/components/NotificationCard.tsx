3893b15e1de5a5c7120e0906477a2c4c8a6593b8
Phu Chau <chauanphu@gmail.com>
2024-10-23 06:48:16 +0700

    auth: authenticate websocket notifications

--- app/components/NotificationCard.tsx
+++ app/components/NotificationCard.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { NEXT_PUBLIC_WS_URL } from '@/lib/api';
 import { motion, AnimatePresence } from 'framer-motion';
+import Cookies from 'js-cookie';
 
 type Notification = {
   id: number;
@@ -12,11 +13,14 @@ type Notification = {
 
 const NotificationCard = () => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
-  const [nextId, setNextId] = useState(1);
 
   useEffect(() => {
     // Establish a WebSocket connection
-    const socket = new WebSocket(`${NEXT_PUBLIC_WS_URL}/notifications`);
+    const token = Cookies.get('token') || '';
+    if (!token) {
+      return;
+    }
+    const socket = new WebSocket(`${NEXT_PUBLIC_WS_URL}/notifications?token=${token}`);
 
     // Handle incoming messages from the WebSocket
     socket.onmessage = (event) => {
@@ -25,7 +29,6 @@ const NotificationCard = () => {
         return;
       }
       setNotifications(data);
-      console.log('Notifications:', notifications);
     };
 
     // Clean up the WebSocket connection when the component unmounts
@@ -54,9 +57,9 @@ const NotificationCard = () => {
   return (
     <div className="fixed bottom-5 right-5 flex flex-col items-start space-y-4 z-50">
       <AnimatePresence>
-        {notifications.map(notification => (
+        {notifications.map((notification, index) => (
           <motion.div
-            key={notification.id}
+            key={index}
             className={`w-80 p-4 rounded shadow-lg ${getNotificationStyle(notification.type)} relative`}
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
